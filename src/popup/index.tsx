ef1e7a978c139f34a6ec3dd4d38c598405912b58
jingyu <jingyu@gaoding.com>
2024-07-04 21:19:44 +0800

    feat: 增加 eslint

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,46 +1,35 @@
-import { sendCurrentTabMessage } from "~services/message";
+import React from "react"
 
-function IndexContent() {
+import { sendCurrentTabMessage } from "~services/message"
 
-  
-
-  
+function Popup() {
   function showSelectCapture() {
-    // sendCurrentTabMessage({ type: "showSelectCapture" });
-    // console.log('showSelectCapture');
-    chrome.tabs.create({ url: chrome.runtime.getURL("tabs/kkk.html") });
-    // window.close();
+    sendCurrentTabMessage({ type: "showSelectCapture" })
   }
 
   function captureCurrent() {
-    sendCurrentTabMessage({ type: "captureCurrent" });
-    window.close();
+    sendCurrentTabMessage({ type: "captureCurrent" })
+    window.close()
   }
 
   function captureFullPage() {
-    sendCurrentTabMessage({ type: "captureFullPage" });
-    window.close();
+    sendCurrentTabMessage({ type: "captureFullPage" })
+    window.close()
   }
 
   return (
     <div className="btn-wrap">
-        <button
-          className="btn"
-          onClick={showSelectCapture}>
-          <p>选定区域截图</p>
-        </button>
-        <button
-          className="btn"
-          onClick={captureCurrent}>
-          <p>可视区域截图</p>
-        </button>
-        <button
-          className="btn"
-          onClick={captureFullPage}>
-          <p>整张截图</p>
-        </button>
-      </div>
-  );
+      <button className="btn" onClick={showSelectCapture}>
+        <p>选定区域截图</p>
+      </button>
+      <button className="btn" onClick={captureCurrent}>
+        <p>可视区域截图</p>
+      </button>
+      <button className="btn" onClick={captureFullPage}>
+        <p>整张截图</p>
+      </button>
+    </div>
+  )
 }
 
-export default IndexContent;
+export default Popup
--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,14 +1,14 @@
 export function getBase64Size(base64: string) {
-  const str = base64.split(",")[1].split("=")[0];
-  const strLength = str.length;
-  const size = Math.ceil(strLength - (strLength / 8) * 2);
+  const str = base64.split(",")[1].split("=")[0]
+  const strLength = str.length
+  const size = Math.ceil(strLength - (strLength / 8) * 2)
   // 单位为m
-  return +(size / 1024 / 1024).toFixed(2);
+  return +(size / 1024 / 1024).toFixed(2)
 }
 
 export const isBase64 = (url: string) => {
   if (!url) {
-    return false;
+    return false
   }
-  return url.startsWith("data:image");
-};
+  return url.startsWith("data:image")
+}
